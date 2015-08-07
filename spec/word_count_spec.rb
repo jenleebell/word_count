992e2cdb79149c7ffaa0333152367cca535b6d4e
Jennifer Bell <student@epicodus.com>
2015-08-07 09:34:48 -0700

    Update first spec with corrected sentence

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -2,7 +2,7 @@ require('rspec')
 require('word_count')
 
 describe('String#word_count') do
-  it("scans the sentence for the given word and returns the word into an array") do
+  it("scans the sentence for the given word and returns the word") do
     expect("hello world".word_count("world")).to(eq("world"))
   end
 end
