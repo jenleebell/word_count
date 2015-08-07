40e51590628135f75b7d38d63c1417f9edb360e3
Jennifer Bell <student@epicodus.com>
2015-08-07 09:36:52 -0700

    Pass second spec successfully

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,7 @@
 class String
   define_method(:word_count) do |check|
 
-    self.scan(check).join
+    self.scan(check).join(" ")
 
 
   end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -5,4 +5,8 @@ describe('String#word_count') do
   it("scans the sentence for the given word and returns the word") do
     expect("hello world".word_count("world")).to(eq("world"))
   end
+
+  it("scans the sentence for the given word and returns the word if it appears more than once") do
+    expect("hello world it feels good to be in this world".word_count("world")).to(eq("world world"))
+  end
 end
